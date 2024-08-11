6dacb291cff704b2527b39a214f326ccf40f2a24
sumitmehta2305 <mehtasumit1432@gmail.com>
2024-08-11 22:35:42 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,38 @@
+# # Example of Assignment Operator 
+
+# # addition
+# x=23
+# x+=17
+# print("Addition of 23 & 17 is",x)
+
+
+# # Subtraction
+# x=36
+# x-=13
+# print("Subtraction of 36 & 13 is",x)
+
+# #Multiplication
+# x=12
+# x*=7
+# print("Multiplication of 12 & 7 is",x)
+
+# #Float Division
+# x=91
+# x/=17
+# print("Division of 91 & 17 is",x)
+
+# # Int division
+
+# x=91
+# x//=17
+# print("Int division of 23 & 17 is",x)
+
+# #Exponetial Oprator
+# x=12
+# x**=2
+# print("12 to the power 2 is",x)
+
+# #reminder
+# x=52
+# x%=7
+# print("reminder of 52 % 7 is",x)
